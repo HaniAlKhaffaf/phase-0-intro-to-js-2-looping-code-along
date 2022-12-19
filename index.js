ceb2b54cbaf9a17e93482a3f5e71876340b15a25
Hani Amer <cvba2222@gmail.com>
2022-12-19 22:06:26 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+    }
+    return messages
+}
+
+function countDown(count) {
+    while (count >= 0) {
+        console.log(count);
+        count--;
+    }
+}
